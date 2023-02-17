73d07c3934ebceed9372b1288f65e87ccdac8e79
creepereye <creepereye1204@gmail.com>
2023-02-17 17:50:34 +0900

    This is the solution to Baekjun 1339 problem

--- 6019/6019.py
+++ 6019/6019.py
@@ -1,19 +1,7 @@
-IN_1=input()
-N,L=IN_1.split(" ")
-N=int(N)
-L=int(L)
-
-IN_2=input()
-I=IN_2.split(" ")
-I=list(map(int,I))
-
-I.sort()
-C=0
-T=0
-
-
+N,L=(map(int,input().split()))
+I=(list(map(int,input()))).sort()
 T=I[0]+L-1
-C+=1
+C=1
 for i in range(1,len(I)):
     if T<I[i]:
         C+=1
